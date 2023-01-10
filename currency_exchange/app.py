7e58f85aefa562314e0f8eb9014ec01c6c787e32
puyan <p.behdarvandi@gmail.com>
2023-01-10 23:07:22 +0100

    make the requested changes in PR

--- currency_exchange/app.py
+++ currency_exchange/app.py
@@ -2,31 +2,20 @@
 import argparse
 import requests
 
-CURRENCIES = {
-    'EUR': 4.75,
-    'USD': 4.80,
-    'CAD': 3.50
-}
-
 
 def parser():
     parser = argparse.ArgumentParser(description='Exchange the currencies')
 
-    parser.add_argument('amount', type=int,
-                        help='an integer for the currency')
-    parser.add_argument('currency', type=str,
-                        choices=CURRENCIES.keys(), help='currency type')
+    parser.add_argument('amount', type=int, help='an integer for the currency')
+    parser.add_argument('currency', type=str, help='currency type')
 
     args = parser.parse_args()
 
     return args
 
 
-args = parser()
-
-
 def fetch_currency_rate(currency: str) -> float:
-    r = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/A/{currency}/')
+    r = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/B/{currency}/')
 
     rates = r.json()
     if rates['code'] == currency:
@@ -34,10 +23,15 @@ def fetch_currency_rate(currency: str) -> float:
 
 
 def convert(value: float, currency: str) -> float:
-    exchange_rate = fetch_currency_rate(currency)
-    return round(value * exchange_rate, 2)
+    try:
+        exchange_rate = fetch_currency_rate(currency)
+        return round(value * exchange_rate, 2)
+    except:
+        print(f"no data available for {currency}")
 
 
-input = (args.amount, args.currency)
-output = convert(value=input[0], currency=input[1])
-print(f"{output} PLN")
+if __name__ == '__main__':
+    args = parser()
+    output = convert(value=args.amount, currency=args.currency)
+    if output is not None:
+        print(f"{output} PLN")
